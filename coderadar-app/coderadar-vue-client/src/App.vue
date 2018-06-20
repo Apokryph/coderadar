506e9268978083906b0e926b9cb66bef6df9f363
Nicole Kobs <nicole.kobs@adesso.de>
2018-06-20 19:08:25 +0200

    added coderadar-app module that is client application based on vue.js with prototyping frameworks like bootstrap-vue

---
+++ coderadar-app/coderadar-vue-client/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <b-nav tabs>
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/project-settings">Project Settings</b-nav-item>
+      </b-nav>
+    </div>
+    <div id="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  #nav {
+    padding: 30px;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+
+  #container {
+    width: 900px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+</style>
---
+++ coderadar-app/coderadar-vue-client/src/views/ProjectSettingsView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="project-settings">
+        <ProjectSettings headline="Project Settings"/>
+    </div>
+</template>
+
+<script>
+    import ProjectSettings from '@/components/ProjectSettings.vue'; // @ is an alias to /src
+
+    export default {
+        name: 'projectSettingsView',
+        components: {
+            ProjectSettings
+        }
+    }
+</script>
